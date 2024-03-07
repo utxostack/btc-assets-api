25b0fde18c2f6c44c11c7f13b8f2db444f62593a
ahonn <ahonn95@outlook.com>
2024-03-07 15:47:14 +1100

    fix: fix type issues

--- src/@types/fastify/index.d.ts
+++ src/@types/fastify/index.d.ts
@@ -1,7 +1,13 @@
 import { AwilixContainer, Cradle } from '../../container';
+import Bitcoind from '../../services/bitcoind';
+import ElectrsAPI from '../../services/electrs';
+import TransactionManager from '../../services/transaction-manager';
+import Paymaster from '../../services/paymaster';
 
 declare module 'fastify' {
-  export interface FastifyInstance extends FastifyJwtNamespace<{ namespace: 'security' }> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  export interface FastifyInstance<HttpServer = Server, HttpRequest = IncomingMessage, HttpResponse = ServerResponse>
+    extends FastifyJwtNamespace<{ namespace: 'security' }> {
     container: AwilixContainer<Cradle>;
     electrs: ElectrsAPI;
     bitcoind: Bitcoind;
