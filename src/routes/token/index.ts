ef6b241fdb2ef8ed164cc6acd427fad2521e8db8
ahonn <ahonn95@outlook.com>
2024-03-07 11:29:02 +1100

    fix: fix token route type

--- src/routes/token/index.ts
+++ src/routes/token/index.ts
@@ -1,13 +1,9 @@
-import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import { FastifyPluginCallback } from 'fastify';
 import { Server } from 'http';
 import generateRoute from './generate';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
 
-const tokenRoutes: FastifyPluginCallback<Record<never, never>, Server, TypeBoxTypeProvider> = (
-  fastify,
-  _,
-  done,
-) => {
+const tokenRoutes: FastifyPluginCallback<Record<never, never>, Server, ZodTypeProvider> = (fastify, _, done) => {
   fastify.register(generateRoute);
   done();
 };
