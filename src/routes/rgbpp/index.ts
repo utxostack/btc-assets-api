cf6309fa88e461117347283a15155970c5ab6665
ahonn <ahonn95@outlook.com>
2024-03-07 15:47:14 +1100

    fix: fix type issues

--- src/routes/rgbpp/index.ts
+++ src/routes/rgbpp/index.ts
@@ -1,12 +1,11 @@
 import { FastifyPluginCallback } from 'fastify';
 import { Server } from 'http';
 import container from '../../container';
-import TransactionQueue from '../../services/transaction-queue';
 import transactionRoutes from './transaction';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
 
 const rgbppRoutes: FastifyPluginCallback<Record<never, never>, Server, ZodTypeProvider> = (fastify, _, done) => {
-  fastify.decorate('transactionManager', container.resolve<TransactionQueue>('transactionManager'));
+  fastify.decorate('transactionManager', container.resolve('transactionManager'));
   fastify.register(transactionRoutes, { prefix: '/transaction' });
   done();
 };
