630120e0dbb333ca3e4f382e1659fc70e5e180c5
ahonn <ahonn95@outlook.com>
2024-03-07 11:37:45 +1100

    fix: update rgbpp routes schemas

--- src/routes/rgbpp/index.ts
+++ src/routes/rgbpp/index.ts
@@ -1,11 +1,11 @@
-import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import { FastifyPluginCallback } from 'fastify';
 import { Server } from 'http';
 import container from '../../container';
 import TransactionQueue from '../../services/transaction-queue';
 import transactionRoutes from './transaction';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
 
-const rgbppRoutes: FastifyPluginCallback<Record<never, never>, Server, TypeBoxTypeProvider> = (fastify, _, done) => {
+const rgbppRoutes: FastifyPluginCallback<Record<never, never>, Server, ZodTypeProvider> = (fastify, _, done) => {
   fastify.decorate('transactionQueue', container.resolve<TransactionQueue>('transactionQueue'));
   fastify.register(transactionRoutes, { prefix: '/transaction' });
   done();
