400e6aa3af408aa6a752ad36966e6f09de1d7ee9
ahonn <yuexunjiang@gmail.com>
2024-03-21 18:01:02 +1100

    fix: upgrade unlocker buildBtcTimeCellsSpentTx

--- src/services/unlocker.ts
+++ src/services/unlocker.ts
@@ -1,19 +1,29 @@
 import { Cradle } from '../container';
-import { buildBtcTimeCellsSpentTx, Collector, IndexerCell, sendCkbTx } from '@rgbpp-sdk/ckb';
+import {
+  BtcTimeCellPair,
+  buildBtcTimeCellsSpentTx,
+  Collector,
+  IndexerCell,
+  sendCkbTx,
+  SPVService,
+  BTCTimeLock,
+} from '@rgbpp-sdk/ckb';
 import { genRgbppLockScript } from '@rgbpp-sdk/ckb/lib/utils/rgbpp';
-import { Cell, CellCollector } from '@ckb-lumos/lumos';
+import { CellCollector } from '@ckb-lumos/lumos';
 
 interface IUnlocker {}
 
 export default class Unlocker implements IUnlocker {
   private cradle: Cradle;
   private collector: CellCollector;
+  private spvService: SPVService;
 
   constructor(cradle: Cradle) {
     this.cradle = cradle;
     this.collector = this.cradle.ckbIndexer.collector({
       lock: this.lockScript,
     }) as CellCollector;
+    this.spvService = new SPVService(this.cradle.env.TRANSACTION_SPV_SERVICE_URL);
   }
 
   private get isMainnet() {
@@ -26,10 +36,23 @@ export default class Unlocker implements IUnlocker {
 
   private async getNextBatchLockCell() {
     const collect = this.collector.collect();
-    const cells: Cell[] = [];
+    const cells: IndexerCell[] = [];
+
+    const { blocks } = await this.cradle.bitcoind.getBlockchainInfo();
     for await (const cell of collect) {
-      // TODO: check cell lock args and skip if btc tx not confirmed 6 blocks yet
-      cells.push(cell);
+      // check cell lock args (after, txid) and skip if btc tx not confirmed $after blocks yet
+      const { after, btcTxid } = BTCTimeLock.unpack(cell.cellOutput.lock.args);
+      const { blockheight } = await this.cradle.bitcoind.getTransaction(btcTxid);
+      if (!blockheight || blocks - blockheight < after) {
+        continue;
+      }
+      cells.push({
+        blockNumber: cell.blockNumber!,
+        outPoint: cell.outPoint!,
+        output: cell.cellOutput,
+        outputData: cell.data,
+        txIndex: cell.txIndex!,
+      });
       if (cells.length >= this.cradle.env.UNLOCKER_CELL_BATCH_SIZE) {
         break;
       }
@@ -43,8 +66,20 @@ export default class Unlocker implements IUnlocker {
       return;
     }
 
+    const btcTimeCellPairs = await Promise.all(
+      cells.map(async (cell) => {
+        const { btcTxid } = BTCTimeLock.unpack(cell.output.lock.args);
+        const { blockindex } = await this.cradle.bitcoind.getTransaction(btcTxid);
+        return {
+          btcTimeCell: cell,
+          btcTxIndexInBlock: blockindex,
+        } as BtcTimeCellPair;
+      }),
+    );
+
     const signedTx = await buildBtcTimeCellsSpentTx({
-      btcTimeCells: cells as unknown as IndexerCell[],
+      btcTimeCellPairs,
+      spvService: this.spvService,
       isMainnet: this.isMainnet,
     });
     const txHash = await sendCkbTx({
