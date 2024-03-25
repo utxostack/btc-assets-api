9715e25abd7c70ab9844e1caceaa3311b6a46fdc
ahonn <yuexunjiang@gmail.com>
2024-03-26 10:45:27 +1100

    feat: update cron task route schema

--- src/routes/cron/transactions.ts
+++ src/routes/cron/transactions.ts
@@ -5,24 +5,37 @@ import { ZodTypeProvider } from 'fastify-type-provider-zod';
 import container from '../../container';
 import TransactionManager from '../../services/transaction';
 
-const transactionsCronRoute: FastifyPluginCallback<Record<never, never>, Server, ZodTypeProvider> = (fastify, _, done) => {
-  fastify.get('/transactions', async () => {
-    const logger = container.resolve<pino.BaseLogger>('logger');
-    const transactionManager: TransactionManager = container.resolve('transactionManager');
-    await Promise.race([
-      transactionManager.startProcess({
-        onActive: (job) => {
-          logger.info(`Job active: ${job.id}`);
-        },
-        onCompleted: (job) => {
-          logger.info(`Job completed: ${job.id}`);
-        },
-      }),
-      new Promise((resolve) => setTimeout(resolve, 59_000)),
-    ]);
-    await transactionManager.pauseProcess();
-    await transactionManager.closeProcess();
-  });
+const transactionsCronRoute: FastifyPluginCallback<Record<never, never>, Server, ZodTypeProvider> = (
+  fastify,
+  _,
+  done,
+) => {
+  fastify.get(
+    '/transactions',
+    {
+      schema: {
+        tags: ['Cron Task'],
+        description: 'Run RGB++ CKB transaction cron task, used for serverless deployment',
+      },
+    },
+    async () => {
+      const logger = container.resolve<pino.BaseLogger>('logger');
+      const transactionManager: TransactionManager = container.resolve('transactionManager');
+      await Promise.race([
+        transactionManager.startProcess({
+          onActive: (job) => {
+            logger.info(`Job active: ${job.id}`);
+          },
+          onCompleted: (job) => {
+            logger.info(`Job completed: ${job.id}`);
+          },
+        }),
+        new Promise((resolve) => setTimeout(resolve, 59_000)),
+      ]);
+      await transactionManager.pauseProcess();
+      await transactionManager.closeProcess();
+    },
+  );
   done();
 };
 
