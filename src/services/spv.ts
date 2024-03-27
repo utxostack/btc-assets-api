af8aef1fd149b03ad00deba1cdb6754a1c272255
Yuexun <ahonn95@outlook.com>
2024-03-27 19:47:15 +1100

    Fix spv service jsonrpc error (#14)

--- src/services/spv.ts
+++ src/services/spv.ts
@@ -56,22 +56,18 @@ export default class BitcoinSPV {
 
   private async callMethod<T>(method: string, params: unknown): Promise<T> {
     return Sentry.startSpan({ op: this.constructor.name, name: method }, async () => {
-      try {
-        const id = randomUUID();
-        const response = await this.request.post('', {
-          jsonrpc: '2.0',
-          id,
-          method,
-          params,
-        });
-        return response.data.result;
-      } catch (err) {
-        if (err instanceof axios.AxiosError && err.response?.data.error) {
-          const { code, message } = BitcoinSPVError.schema.parse(err.response.data.error);
-          throw new BitcoinSPVError(code, message);
-        }
-        throw err;
+      const id = randomUUID();
+      const response = await this.request.post('', {
+        jsonrpc: '2.0',
+        id,
+        method,
+        params,
+      });
+      if (response.data?.error) {
+        const { code, message } = BitcoinSPVError.schema.parse(response.data.error);
+        throw new BitcoinSPVError(code, message);
       }
+      return response.data?.result;
     });
   }
 
