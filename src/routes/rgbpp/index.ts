0ade788d8a27944ef37239aec8a9217eecc13f61
ahonn <yuexunjiang@gmail.com>
2024-03-27 14:33:31 +1100

    feat: show paymaster routes

--- src/routes/rgbpp/index.ts
+++ src/routes/rgbpp/index.ts
@@ -6,6 +6,7 @@ import { ZodTypeProvider } from 'fastify-type-provider-zod';
 import assetsRoute from './assets';
 import addressRoutes from './address';
 import spvRoute from './spv';
+import paymasterRoutes from './paymaster';
 
 const rgbppRoutes: FastifyPluginCallback<Record<never, never>, Server, ZodTypeProvider> = (fastify, _, done) => {
   fastify.decorate('transactionManager', container.resolve('transactionManager'));
@@ -19,6 +20,7 @@ const rgbppRoutes: FastifyPluginCallback<Record<never, never>, Server, ZodTypePr
   fastify.register(assetsRoute, { prefix: '/assets' });
   fastify.register(addressRoutes, { prefix: '/address' });
   fastify.register(spvRoute, { prefix: '/btc-spv' });
+  fastify.register(paymasterRoutes, { prefix: '/paymaster' });
   done();
 };
 
