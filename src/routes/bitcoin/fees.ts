649df0fd2780d35a9d7bebbd4a64f163a9644a8e
ahonn <yuexunjiang@gmail.com>
2024-04-28 14:44:25 +1000

    feat: server cache bitcoin recommended fees 10 seconds

--- src/routes/bitcoin/fees.ts
+++ src/routes/bitcoin/fees.ts
@@ -2,6 +2,7 @@ import { FastifyPluginCallback } from 'fastify';
 import { Server } from 'http';
 import { RecommendedFees } from './types';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { CUSTOM_HEADERS } from '../../constants';
 
 const feesRoutes: FastifyPluginCallback<Record<never, never>, Server, ZodTypeProvider> = (fastify, _, done) => {
   fastify.get(
@@ -15,8 +16,10 @@ const feesRoutes: FastifyPluginCallback<Record<never, never>, Server, ZodTypePro
         },
       },
     },
-    async () => {
+    async (_, reply) => {
       const fees = await fastify.bitcoin.getFeesRecommended();
+      reply.header(CUSTOM_HEADERS.ResponseCacheable, 'true');
+      reply.header(CUSTOM_HEADERS.ResponseCacheMaxAge, 10);
       return fees;
     },
   );
