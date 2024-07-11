50cd25e64f748c240813d29002896d7a6f05aa63
Shook <shooklyngs@gmail.com>
2024-07-11 23:29:27 +0800

    refactor: migrate swagger to use openapi@3.1.0, fixing the oneOf/anyOf rendering issue in the docs

--- src/plugins/swagger.ts
+++ src/plugins/swagger.ts
@@ -11,36 +11,36 @@ export const DOCS_ROUTE_PREFIX = '/docs';
 export default fp(async (fastify) => {
   fastify.register(swagger, {
     hideUntagged: true,
-    swagger: {
+    openapi: {
+      openapi: '3.1.0',
       info: {
         title: 'Bitcoin/RGB++ Assets API',
         version: pkg.version,
       },
-      consumes: ['application/json'],
-      produces: ['application/json'],
       security: [{ apiKey: [] }],
-      securityDefinitions: {
-        apiKey: {
-          type: 'apiKey',
-          name: 'Authorization',
-          in: 'header',
-          description: 'JWT token for authentication. Example: Bearer <token>',
+      components: {
+        securitySchemes: {
+          apiKey: {
+            type: 'apiKey',
+            name: 'Authorization',
+            in: 'header',
+            description: 'JWT token for authentication. Example: Bearer <token>',
+          },
         },
       },
     },
     transform: jsonSchemaTransform,
-    transformObject: ({ swaggerObject }) => {
+    transformObject: ({ openapiObject }) => {
       if (env.NODE_ENV === 'production') {
-        const { paths = {} } = swaggerObject;
-        const newPaths = Object.entries(paths).reduce((acc, [path, methods]) => {
+        const { paths = {} } = openapiObject;
+        openapiObject.paths = Object.entries(paths).reduce((acc, [path, methods]) => {
           if (SWAGGER_PROD_IGNORE_URLS.some((ignorePath) => path.startsWith(ignorePath))) {
             return acc;
           }
           return { ...acc, [path]: methods };
         }, {});
-        swaggerObject.paths = newPaths;
       }
-      return swaggerObject;
+      return openapiObject;
     },
   });
   fastify.register(swaggerUI, {
